910f9b5f1a091f1cf9eedaa24f1ddacef7832001
CelaniDe <celanide@gmail.com>
2024-01-29 20:16:37 +0200

    Add dropdown

--- react/src/Pages/Navbar/Navbar.jsx
+++ react/src/Pages/Navbar/Navbar.jsx
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { UserOutlined } from "@ant-design/icons";
 import { Avatar } from "antd";
-import { Button } from "antd";
+import { Button, Dropdown } from "antd";
 import { useNavigate } from "react-router-dom";
 import uploadImg from "../../assets/images/upload.png";
+import AuthContext from "../AuthContext/AuthContext";
 
 const App = () => {
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleMenuClick = (e) => {
+    if (e.key === '1') {
+      // setOpen(false);
+      navigate('/');
+    }
+    else if (e.key === '2') {
+      // setOpen(false);
+      logout();
+    }
+  };
+
+
+  const items = [
+    {
+      key: '1',
+      label: 'Library'
+    },
+    {
+      key: '2',
+      label: 'Logout'
+    },
+  ];
+
+
   const [isHovered, setIsHovered] = useState(false);
 
-  const navigate = useNavigate();
+  
 
   const openLibrary = () => {
     navigate("/");
@@ -33,22 +61,24 @@ const App = () => {
         <img src={uploadImg} alt="upload image" style={{height: '12px', marginRight: '7px'}} />
         Upload
       </Button>
-      <Avatar
-        style={{
-          transition: "filter 0.2s linear",
-          marginLeft: "30px",
-          marginTop: '-3px',
-          // border: "1px solid lightblue",
-          backgroundColor: 'rgba(0, 207, 222, 1)',
-          cursor: isHovered ? "pointer" : "default",
-          filter: isHovered ? "drop-shadow(1px 1px 2px #6b6b6b)" : "none",
-        }}
-        size="large"
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        onClick={openLibrary}
-        icon={<UserOutlined />}
-      />
+      <Dropdown menu={{items,onClick : handleMenuClick}}>
+        <Avatar
+          style={{
+            transition: "filter 0.2s linear",
+            marginLeft: "30px",
+            marginTop: '-3px',
+            // border: "1px solid lightblue",
+            backgroundColor: 'rgba(0, 207, 222, 1)',
+            cursor: isHovered ? "pointer" : "default",
+            filter: isHovered ? "drop-shadow(1px 1px 2px #6b6b6b)" : "none",
+          }}
+          size="large"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          // onClick={openLibrary}
+          icon={<UserOutlined />}
+        />
+      </Dropdown>
     </div>
   );
 };
